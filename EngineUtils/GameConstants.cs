386b0134a16ef8b358632e0c6f01308523cb28ea
Cedric Hippmann <cedric.hippmann@hotmail.com>
2019-11-04 10:12:11 +0100

    Only show hover if in actionRadius

--- EngineUtils/GameConstants.cs
+++ EngineUtils/GameConstants.cs
@@ -3,5 +3,6 @@ namespace GameOff_2019.EngineUtils {
         public const int GameUnitSize = 128;
         public const string PlayerGroup = "player";
         public const string PathfindingTileMapGroup = "pathfindingTileMap";
+        public const string ActionRadiusGroup = "actionRadius";
     }
 }
--- Levels/Common/TileMapObjects/BaseObject/HoverIndicator.cs
+++ Levels/Common/TileMapObjects/BaseObject/HoverIndicator.cs
@@ -1,12 +1,21 @@
 using System;
+using System.Collections.Generic;
 using GameOff_2019.EngineUtils;
 using GameOff_2019.Entities.Common.Navigation;
 using Godot;
 
 public class HoverIndicator : Sprite {
+    [Export] private readonly NodePath actionRadiusColliderNodePath = null;
+    private Area2D actionRadiusCollider;
+
     private PathfindingTileMap pathfindingTileMap;
+    private readonly List<Node2D> collidingActionRadiusColliderList = new List<Node2D>();
 
     public override void _Ready() {
+        actionRadiusCollider = GetNode<Area2D>(actionRadiusColliderNodePath);
+        actionRadiusCollider.Connect("area_entered", this, nameof(OnAreaEntered));
+        actionRadiusCollider.Connect("area_exited", this, nameof(OnAreaExited));
+
         var tileMaps = GetTree().GetNodesInGroup(GameConstants.PathfindingTileMapGroup);
         if (tileMaps.Count != 1) {
             throw new Exception("There should be exactly one pathfindingTileMap in the sceneTree!");
@@ -21,6 +30,18 @@ public class HoverIndicator : Sprite {
     }
 
     public override void _Process(float delta) {
-        Visible = pathfindingTileMap.WorldToMap(GetGlobalMousePosition()) == pathfindingTileMap.WorldToMap(GetGlobalPosition());
+        Visible = pathfindingTileMap.WorldToMap(GetGlobalMousePosition()) == pathfindingTileMap.WorldToMap(GetGlobalPosition()) && collidingActionRadiusColliderList.Count > 0;
+    }
+
+    private void OnAreaEntered(Node2D body) {
+        if (body.IsInGroup(GameConstants.ActionRadiusGroup)) {
+            collidingActionRadiusColliderList.Add(body);
+        }
+    }
+
+    private void OnAreaExited(Node2D body) {
+        if (body.IsInGroup(GameConstants.ActionRadiusGroup)) {
+            collidingActionRadiusColliderList.Remove(body);
+        }
     }
 }
