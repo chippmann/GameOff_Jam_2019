60ad4d49478ed521a5499444b590cd184814f002
Cedric Hippmann <cedric.hippmann@hotmail.com>
2019-11-29 22:13:03 +0100

    Implement Player walk animations

--- Entities/Common/Movement/EntityMovement.cs
+++ Entities/Common/Movement/EntityMovement.cs
@@ -21,7 +21,7 @@ namespace GameOff_2019.Entities.Common.Movement {
         private int currentTargetNode = -1;
         private int stuckFrames;
         private readonly int maxStuckFrames = 20;
-        private readonly int entityMaxPositionErrorInPixels = 2;
+        private readonly int entityMaxPositionErrorInPixels = 4;
         private Vector2 oldPosition;
 
 
@@ -113,6 +113,9 @@ namespace GameOff_2019.Entities.Common.Movement {
             }
 
             velocity = entity.MoveAndSlide(velocity);
+            if (internalIsPlayer) {
+                Logger.Debug("Velocity: " + velocity);
+            }
         }
 
         private bool IsMovingDiagonally() {
@@ -220,6 +223,10 @@ namespace GameOff_2019.Entities.Common.Movement {
         private bool ReachedNodeOnYAxis(Vector2 previousDestination, Vector2 currentDestination) {
             return (previousDestination.y <= currentDestination.y && Mathf.Round(GetGlobalPosition().y + entityMaxPositionErrorInPixels) >= currentDestination.y)
                    || (previousDestination.y >= currentDestination.y && Mathf.Round(GetGlobalPosition().y - entityMaxPositionErrorInPixels) <= currentDestination.y);
+        }
+
+        public Vector2 GetVelocity() {
+            return velocity;
         }
     }
 }
--- Entities/PlayerEntity/Animation/PlayerAnimationManager.cs
+++ Entities/PlayerEntity/Animation/PlayerAnimationManager.cs
@@ -1,3 +1,70 @@
+using System;
+using GameOff_2019.Entities.Common.Movement;
+using Godot;
+
 namespace GameOff_2019.Entities.PlayerEntity.Animation {
-    public class PlayerAnimationManager { }
+    public class PlayerAnimationManager : AnimatedSprite {
+        [Export] private readonly NodePath entityMovementNodePath = null;
+        private EntityMovement entityMovement;
+
+        public override void _Ready() {
+            base._Ready();
+            entityMovement = GetNode<EntityMovement>(entityMovementNodePath);
+        }
+
+        public override void _Process(float delta) {
+            base._Process(delta);
+
+            var velocity = entityMovement.GetVelocity();
+
+            if (velocity == Vector2.Zero && IsPlaying()) {
+                Stop();
+            }
+            else if (velocity != Vector2.Zero && !IsPlaying()) {
+                var animationToPlay = GetAnimationToPlay(velocity);
+                if (animationToPlay == GetAnimation()) {
+                    Play();
+                }
+                else {
+                    Play(animationToPlay);
+                }
+            }
+        }
+
+        private string GetAnimationToPlay(Vector2 velocity) {
+            if (Math.Abs(velocity.x) < 0.01 && velocity.y < -0.2) { //up
+                return "up";
+            }
+
+            if (Math.Abs(velocity.x) < 0.01 && velocity.y > 0.2) { //down
+                return "down";
+            }
+
+            if (velocity.x < -0.2 && Math.Abs(velocity.y) < 0.01) { //left
+                return "left";
+            }
+
+            if (velocity.x > 0.2 && Math.Abs(velocity.y) < 0.01) { //right
+                return "right";
+            }
+
+            if (velocity.x < -0.2 && velocity.y < -0.2) { //upLeft
+                return "upLeft";
+            }
+
+            if (velocity.x > 0.2 && velocity.y < -0.2) { //upRight
+                return "upRight";
+            }
+
+            if (velocity.x < -0.2 && velocity.y > 0.2) { //downLeft
+                return "downLeft";
+            }
+
+            if (velocity.x > 0.2 && velocity.y > 0.2) { //downRight
+                return "downRight";
+            }
+
+            return GetAnimation();
+        }
+    }
 }
