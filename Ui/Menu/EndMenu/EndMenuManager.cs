ee129c50e83ade297e08b1ab3d7923f9bc4b2237
Josias <josias@macherstube.ch>
2019-12-01 02:41:36 +0100

    Add Loosing and Winning Sounds :)

--- Ui/Menu/EndMenu/EndMenuManager.cs
+++ Ui/Menu/EndMenu/EndMenuManager.cs
@@ -1,6 +1,7 @@
 using Godot;
 using Planty.EngineUtils;
 using Planty.RoundLogic;
+using Planty.SoundEngine;
 
 namespace Planty.Ui.Menu.EndMenu {
     public class EndMenuManager : Control {
@@ -9,11 +10,14 @@ namespace Planty.Ui.Menu.EndMenu {
         [Export] private readonly string worldLimitTitleText = "";
         [Export] private readonly string worldLimitMessageText = "";
         [Export] private readonly NodePath titleLabelNodePath = null;
+        [Export] private readonly AudioStreamOGGVorbis loosingSound = null;
+        [Export] private readonly AudioStreamOGGVorbis winningSound = null;
         private Label titleLabel;
         [Export] private readonly NodePath messageLabelNodePath = null;
         private Label messageLabel;
         [Export] private readonly NodePath continueButtonNodePath = null;
         private Button continueButton;
+        private SoundEngineNode soundEngineNode;
 
         public override void _Ready() {
             base._Ready();
@@ -21,6 +25,14 @@ namespace Planty.Ui.Menu.EndMenu {
             messageLabel = GetNode<Label>(messageLabelNodePath);
             continueButton = GetNode<Button>(continueButtonNodePath);
             continueButton.Connect("pressed", this, nameof(OnContinueButtonPressed));
+
+            soundEngineNode = NodeGetter.GetFirstNodeInGroup<SoundEngineNode>(GetTree(), GameConstants.SoundEngineGroup, true);
+            if (pointsLimitTitleText.EndsWith("won")) {
+                soundEngineNode.PlaySfx(winningSound, this);
+            }
+            else {   
+                soundEngineNode.PlaySfx(loosingSound, this);
+            }
 
             var gameState = NodeGetter.GetFirstNodeInGroup<GameState>(GetTree(), GameConstants.GameStateGroup, true);
             if (gameState.isPointLimitEnabled) {
